03146bf1db625158faac0198e1b6d057054ea718
Perin <joaovperin@live.com>
2020-12-01 21:23:51 -0300

    improved user pic showing to counsider sex

--- lib/person_list_provider.dart
+++ lib/person_list_provider.dart
@@ -8,6 +8,7 @@ import 'person.dart';
 
 class EntityListProvider with ChangeNotifier {
   final List<Person> _list = [];
+  static const kTableName = 'Person';
 
   Future<void> loadList() async {
     _list
@@ -31,27 +32,30 @@ class EntityListProvider with ChangeNotifier {
   }
 
   Future<void> saveOnDatabase(Person p) async {
-    await DbUtils.insert('Person', p.toMap());
+    await DbUtils.insert(kTableName, p.toMap());
   }
 
   Future<List<Person>> loadFromDatabase() async {
-    final list = await DbUtils.query('Person');
+    final list = await DbUtils.query(kTableName);
     return list.map((e) => Person.fromMap(e)).toList();
   }
 
   Future<void> removeAllFromDatabase() async {
-    await DbUtils.delete('Person');
+    await DbUtils.delete(kTableName);
   }
 
   Future<void> removeOneFromDatabase(Person p) async {
-    await DbUtils.delete('Person', where: 'id = ?', args: [p.id]);
+    await DbUtils.delete(kTableName, where: 'id = ?', args: [p.id]);
   }
 
   Future<List<Person>> getFromWebService() async {
-    var response = await http.get('https://jsonplaceholder.typicode.com/users');
+    var response = await http.get(
+      'https://my-json-server.typicode.com/joaovperin/my_persons/$kTableName',
+    );
     final list = json.decode(response.body) as List;
     if (list.length > 0) {
       return list
+          // Normaliza os registros
           .map((elm) => Person.fromMap({
                 ...elm,
                 'id': null,
@@ -59,7 +63,7 @@ class EntityListProvider with ChangeNotifier {
                     '${elm["address"]["city"]} ${elm["address"]["street"]}',
                 'address2': elm["address"]["suite"],
                 'photo':
-                    'https://randomuser.me/api/portraits/men/${elm["id"]}.jpg'
+                    'https://randomuser.me/api/portraits/${elm["sex"]}/${elm["id"]}.jpg'
               }))
           .toList();
     }
